40f45e11b8d9eb02f9f80f82e3d355f599511335
wwq010917 <wang4633@purdue.edu>
2023-01-05 17:50:01 -0500

    tippecanoe polygons

--- src/GeoJSONGenerate.js
+++ src/GeoJSONGenerate.js
@@ -3,59 +3,90 @@ const merc = new SphericalMercator({
   size: 512,
   antimeridian: true,
 });
-var features = [];
-var origin = [-87.096306,40.214224];
-//[-87.096306,40.214224,-86.694305,40.563003]
-var count = 0;
-for (var i = 0; i < 300; i++) {
-  for (var j = 0; j < 300; j++) {
-    var pixelOrigin = merc.px(origin, 22);
-    const pixelDest1 = [pixelOrigin[0], pixelOrigin[1] - 1405.38];
-    const pixelDest2 = [pixelOrigin[0] + 1405.38, pixelOrigin[1] - 1405.38];
-    const pixelDest3 = [pixelOrigin[0] + 1405.38, pixelOrigin[1]];
-    const cordDest1 = merc.ll(pixelDest1, 22);
-    const cordDest2 = merc.ll(pixelDest2, 22);
-    const cordDest3 = merc.ll(pixelDest3, 22);
-    if (j == 0) {
-      iter = cordDest3;
+
+const fs = require("fs");
+let startstr = `{
+  "type": "FeatureCollection",
+  "features": [
+    `;
+let endstr = `
+  ]
+}`;
+// Open the file for writing
+fs.truncate("polygons.geojson", 0, (error) => {
+  fs.open("polygons.geojson", "w", (error, fd) => {
+    if (error) {
+      console.error(error);
+      return;
     }
-    var coordinates = [[origin, cordDest1, cordDest2, cordDest3, origin],
-    ];
-   // function that returns a random owner
-    
-    var feature = {
-      id: count,
-      geometry: {
-        type: "Polygon",
-        coordinates: coordinates,
-      },
-      type: "Feature",
-      properties:{}
-    };
-    features.push(feature);
-    origin = cordDest1;
-    count++;
-  }
-  origin = iter;
-}
-console.log(count)
-var source = {
-  type: "geojson",
-  data: {
-    type: "FeatureCollection",
-    features: features,
-  },
-};
-const fs = require('fs');
+    fs.appendFile(fd, startstr, (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+    // Start the loop to generate the features
+    //[-87.096306,40.214224,-86.694305,40.563003]
+    var features = [];
+    var origin = [-87.096306, 40.214224];
+    var count = 0;
+    for (var i = 0; i < 2500; i++) {
+      if (origin[0] > -86.694305 || origin[1] > 40.563003) {
+        break;
+      }
+      for (var j = 0; j < 2500; j++) {
+        if (origin[0] > -86.694305 || origin[1] > 40.563003) {
+          break;
+        }
+        var pixelOrigin = merc.px(origin, 22);
+        const pixelDest1 = [pixelOrigin[0], pixelOrigin[1] - 1405.38];
+        const pixelDest2 = [pixelOrigin[0] + 1405.38, pixelOrigin[1] - 1405.38];
+        const pixelDest3 = [pixelOrigin[0] + 1405.38, pixelOrigin[1]];
+        const cordDest1 = merc.ll(pixelDest1, 22);
+        const cordDest2 = merc.ll(pixelDest2, 22);
+        const cordDest3 = merc.ll(pixelDest3, 22);
+        if (j == 0) {
+          iter = cordDest3;
+        }
+        var coordinates = [[origin, cordDest1, cordDest2, cordDest3, origin]];
 
-// Convert the source data to a string in GeoJSON format
-const geojsonString = JSON.stringify(source.data);
+        var feature = {
+          id: count,
+          geometry: {
+            type: "Polygon",
+            coordinates: coordinates,
+          },
+          type: "Feature",
+          properties: {},
+        };
 
-// Write the string to a file asynchronously
-fs.writeFile('polygons.geojson', geojsonString, (error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Successfully written to polygons.geojson');
-  }
+        // Convert the feature to a string in GeoJSON format
+        const featureString = JSON.stringify(feature) + ",";
+
+        // Append the feature string to the file
+        fs.appendFile(fd, featureString, (error) => {
+          if (error) {
+            console.error(error);
+          }
+        });
+
+        origin = cordDest1;
+        count++;
+      }
+      origin = iter;
+    }
+    fs.appendFile(fd, endstr, (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+
+    // Close the file when the loop is done
+    fs.close(fd, (error) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("Successfully written to polygons.geojson");
+      }
+    });
+  });
 });
--- src/Map.js
+++ src/Map.js
@@ -1,29 +1,23 @@
 import mapboxgl from "mapbox-gl";
-import ColorPanel from './color.js'
-import React, { useEffect, useRef,useState } from "react";
+import ColorPanel from "./color.js";
+import React, { useEffect, useRef, useState } from "react";
 import "./Map.css";
-
-
 
 mapboxgl.accessToken =
   "pk.eyJ1Ijoid3dxMDEwOTE3IiwiYSI6ImNsYXIxYjFocjAwNHozdnFxbDlkN2l3anEifQ.cVluyRrnbcBLDZoJMOGysQ";
 
-var iter
+var iter;
 let layer7;
-var color = 'red'
-
+var color = "red";
 
 const Map = () => {
-
-
   const handleColorChange = (selectedColor) => {
-    console.log(selectedColor)
-    color = selectedColor
-    console.log(color)
+    console.log(selectedColor);
+    color = selectedColor;
+    console.log(color);
   };
 
   const mapContainerRef = useRef(null);
-
 
   var origin = [-86.945623, 40.470332];
 
@@ -33,64 +27,65 @@ const Map = () => {
       container: mapContainerRef.current,
       style: "mapbox://styles/wwq010917/clcd19za9008f14qqtna3k281",
       center: origin,
-      zoom: 22, // starting zoom
-      minZoom:2,
+      zoom: 18, // starting zoom
+      minZoom: 3,
     });
-    map.on("load",  () => {
-      
+    map.on("load", () => {
       const layers = map.getStyle().layers;
       console.log(layers);
       for (const layer of layers) {
-      if (layer.id === 'test7') {
-        layer7 = layer;
-      break;
+        if (layer.id === "tippecanoe4") {
+          layer7 = layer;
+          break;
+        }
       }
-      }
-      console.log(layer7)
+      console.log(layer7);
 
       map.addControl(new mapboxgl.NavigationControl());
-  
     });
-    map.on('click', function(e) {
-
-      var features = map.queryRenderedFeatures(e.point, { layers: ['3dtest7'] });
+    map.on("click", function (e) {
+      var features = map.queryRenderedFeatures(e.point, {
+        layers: ["tippecanoe4"],
+      });
       if (!features.length) {
-      return;
+        return;
       }
-      console.log(features)
-      const clickedPolygon = features[0]
-      console.log(clickedPolygon.id)
+      console.log(features);
+      const clickedPolygon = features[0];
+      console.log(clickedPolygon.id);
 
-      map.setPaintProperty("test7", "fill-color", [
+      map.setPaintProperty("tippecanoe4", "fill-color", [
         "case",
         ["!=", ["feature-state", "fillColor"], null],
         ["feature-state", "fillColor"],
         "#f8f8ff",
-        
       ]);
-      map.setPaintProperty("test7", "fill-opacity", [
+      map.setPaintProperty("tippecanoe4", "fill-opacity", [
         "case",
         ["!=", ["feature-state", "fillOpacity"], null],
         ["feature-state", "fillOpacity"],
         0,
       ]);
       map.setFeatureState(
-        { source: "composite", sourceLayer: "test7", id: clickedPolygon.id }, { fillColor: color,fillOpacity:1 });
-      });
-      
+        {
+          source: "composite",
+          sourceLayer: "Tippecanoe4",
+          id: clickedPolygon.id,
+        },
+        { fillColor: color, fillOpacity: 1 }
+      );
+    });
 
-    
     // Clean up on unmount
     return () => map.remove();
   }, []);
 
   return (
     <div>
-       <div className=".color-panel">
+      <div className=".color-panel">
         <ColorPanel onColorChange={handleColorChange} />
       </div>
       <div className="map-container" ref={mapContainerRef} />
-      
     </div>
   );
 };
